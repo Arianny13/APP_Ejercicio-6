805cc70a32da85692f99c564b42f34face62bafa
USUARIO <USUARIO@ARIPC>
2024-03-05 22:33:22 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace APP_Ejercicio_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Calcular = new System.Windows.Forms.Button();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.lbl_Situacion = new System.Windows.Forms.Label();
+            this.lbl_Promedio = new System.Windows.Forms.Label();
+            this.lbl_Nota1 = new System.Windows.Forms.Label();
+            this.lbl_Nota2 = new System.Windows.Forms.Label();
+            this.lbl_Nota3 = new System.Windows.Forms.Label();
+            this.lbl_Nota4 = new System.Windows.Forms.Label();
+            this.txt_Not1 = new System.Windows.Forms.TextBox();
+            this.txt_Nota2 = new System.Windows.Forms.TextBox();
+            this.txt_Nota3 = new System.Windows.Forms.TextBox();
+            this.txt_Situacion = new System.Windows.Forms.TextBox();
+            this.txt_Nota4 = new System.Windows.Forms.TextBox();
+            this.txt_Promedio = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.BackColor = System.Drawing.Color.White;
+            this.btn_Calcular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Calcular.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Calcular.Location = new System.Drawing.Point(98, 393);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(112, 36);
+            this.btn_Calcular.TabIndex = 0;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = false;
+            this.btn_Calcular.Click += new System.EventHandler(this.btn_Calcular_Click);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.BackColor = System.Drawing.Color.White;
+            this.btn_Limpiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Limpiar.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpiar.Location = new System.Drawing.Point(265, 393);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(112, 36);
+            this.btn_Limpiar.TabIndex = 1;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = false;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.BackColor = System.Drawing.Color.White;
+            this.btn_Salir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Salir.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Salir.Location = new System.Drawing.Point(433, 393);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(112, 36);
+            this.btn_Salir.TabIndex = 2;
+            this.btn_Salir.Text = "Salir";
+            this.btn_Salir.UseVisualStyleBackColor = false;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
+            // 
+            // lbl_Situacion
+            // 
+            this.lbl_Situacion.AutoSize = true;
+            this.lbl_Situacion.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Situacion.Location = new System.Drawing.Point(138, 280);
+            this.lbl_Situacion.Name = "lbl_Situacion";
+            this.lbl_Situacion.Size = new System.Drawing.Size(91, 23);
+            this.lbl_Situacion.TabIndex = 3;
+            this.lbl_Situacion.Text = "Situacion";
+            // 
+            // lbl_Promedio
+            // 
+            this.lbl_Promedio.AutoSize = true;
+            this.lbl_Promedio.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Promedio.Location = new System.Drawing.Point(138, 324);
+            this.lbl_Promedio.Name = "lbl_Promedio";
+            this.lbl_Promedio.Size = new System.Drawing.Size(93, 23);
+            this.lbl_Promedio.TabIndex = 4;
+            this.lbl_Promedio.Text = "Promedio";
+            // 
+            // lbl_Nota1
+            // 
+            this.lbl_Nota1.AutoSize = true;
+            this.lbl_Nota1.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nota1.Location = new System.Drawing.Point(138, 107);
+            this.lbl_Nota1.Name = "lbl_Nota1";
+            this.lbl_Nota1.Size = new System.Drawing.Size(62, 23);
+            this.lbl_Nota1.TabIndex = 5;
+            this.lbl_Nota1.Text = "Nota1";
+            // 
+            // lbl_Nota2
+            // 
+            this.lbl_Nota2.AutoSize = true;
+            this.lbl_Nota2.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nota2.Location = new System.Drawing.Point(138, 143);
+            this.lbl_Nota2.Name = "lbl_Nota2";
+            this.lbl_Nota2.Size = new System.Drawing.Size(62, 23);
+            this.lbl_Nota2.TabIndex = 6;
+            this.lbl_Nota2.Text = "Nota2";
+            this.lbl_Nota2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lbl_Nota3
+            // 
+            this.lbl_Nota3.AutoSize = true;
+            this.lbl_Nota3.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nota3.Location = new System.Drawing.Point(138, 181);
+            this.lbl_Nota3.Name = "lbl_Nota3";
+            this.lbl_Nota3.Size = new System.Drawing.Size(62, 23);
+            this.lbl_Nota3.TabIndex = 7;
+            this.lbl_Nota3.Text = "Nota3";
+            this.lbl_Nota3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lbl_Nota4
+            // 
+            this.lbl_Nota4.AutoSize = true;
+            this.lbl_Nota4.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nota4.Location = new System.Drawing.Point(138, 222);
+            this.lbl_Nota4.Name = "lbl_Nota4";
+            this.lbl_Nota4.Size = new System.Drawing.Size(62, 23);
+            this.lbl_Nota4.TabIndex = 8;
+            this.lbl_Nota4.Text = "Nota4";
+            this.lbl_Nota4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txt_Not1
+            // 
+            this.txt_Not1.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Not1.Location = new System.Drawing.Point(279, 98);
+            this.txt_Not1.Name = "txt_Not1";
+            this.txt_Not1.Size = new System.Drawing.Size(144, 32);
+            this.txt_Not1.TabIndex = 9;
+            // 
+            // txt_Nota2
+            // 
+            this.txt_Nota2.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Nota2.Location = new System.Drawing.Point(279, 140);
+            this.txt_Nota2.Name = "txt_Nota2";
+            this.txt_Nota2.Size = new System.Drawing.Size(144, 32);
+            this.txt_Nota2.TabIndex = 10;
+            // 
+            // txt_Nota3
+            // 
+            this.txt_Nota3.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Nota3.Location = new System.Drawing.Point(279, 181);
+            this.txt_Nota3.Name = "txt_Nota3";
+            this.txt_Nota3.Size = new System.Drawing.Size(144, 32);
+            this.txt_Nota3.TabIndex = 11;
+            // 
+            // txt_Situacion
+            // 
+            this.txt_Situacion.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Situacion.Location = new System.Drawing.Point(279, 280);
+            this.txt_Situacion.Name = "txt_Situacion";
+            this.txt_Situacion.Size = new System.Drawing.Size(144, 32);
+            this.txt_Situacion.TabIndex = 12;
+            // 
+            // txt_Nota4
+            // 
+            this.txt_Nota4.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Nota4.Location = new System.Drawing.Point(279, 219);
+            this.txt_Nota4.Name = "txt_Nota4";
+            this.txt_Nota4.Size = new System.Drawing.Size(144, 32);
+            this.txt_Nota4.TabIndex = 13;
+            // 
+            // txt_Promedio
+            // 
+            this.txt_Promedio.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Promedio.Location = new System.Drawing.Point(279, 324);
+            this.txt_Promedio.Name = "txt_Promedio";
+            this.txt_Promedio.Size = new System.Drawing.Size(144, 32);
+            this.txt_Promedio.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(182, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(241, 23);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Situacion del Estudiante";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(625, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_Promedio);
+            this.Controls.Add(this.txt_Nota4);
+            this.Controls.Add(this.txt_Situacion);
+            this.Controls.Add(this.txt_Nota3);
+            this.Controls.Add(this.txt_Nota2);
+            this.Controls.Add(this.txt_Not1);
+            this.Controls.Add(this.lbl_Nota4);
+            this.Controls.Add(this.lbl_Nota3);
+            this.Controls.Add(this.lbl_Nota2);
+            this.Controls.Add(this.lbl_Nota1);
+            this.Controls.Add(this.lbl_Promedio);
+            this.Controls.Add(this.lbl_Situacion);
+            this.Controls.Add(this.btn_Salir);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.btn_Calcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Calcular;
+        private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Button btn_Salir;
+        private System.Windows.Forms.Label lbl_Situacion;
+        private System.Windows.Forms.Label lbl_Promedio;
+        private System.Windows.Forms.Label lbl_Nota1;
+        private System.Windows.Forms.Label lbl_Nota2;
+        private System.Windows.Forms.Label lbl_Nota3;
+        private System.Windows.Forms.Label lbl_Nota4;
+        private System.Windows.Forms.TextBox txt_Not1;
+        private System.Windows.Forms.TextBox txt_Nota2;
+        private System.Windows.Forms.TextBox txt_Nota3;
+        private System.Windows.Forms.TextBox txt_Situacion;
+        private System.Windows.Forms.TextBox txt_Nota4;
+        private System.Windows.Forms.TextBox txt_Promedio;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APP_Ejercicio_7
+{
+    public partial class Form1 : Form
+    {
+        private object txt_Nota1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+            int nota1, nota2, nota3, nota4, promedio;
+            string situacion;
+
+            nota1 = int.Parse(txt_Not1.Text);
+            nota2 = int.Parse(txt_Nota2.Text);
+            nota3 = int.Parse(txt_Nota3.Text);
+            nota4 = int.Parse(txt_Nota4.Text);
+
+            promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+
+
+            if (promedio >= 70)
+            {
+                situacion = "Aprobado";
+                txt_Situacion.ForeColor = Color.Green;
+            }
+            else
+            {
+                situacion = "Reprobado";
+                txt_Situacion.ForeColor = Color.Red;
+            }
+
+
+            txt_Promedio.Text = promedio.ToString();
+            txt_Situacion.Text = situacion;
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            txt_Not1.Clear();
+            txt_Nota2.Clear();
+            txt_Nota3.Clear();
+            txt_Nota4.Text = string.Empty;
+            txt_Promedio.Clear();
+            txt_Situacion.Clear();
+        }
+
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
